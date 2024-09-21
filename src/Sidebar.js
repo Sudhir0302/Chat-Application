76ad560d637284ce44bddcdf82eb017b3aa9f88b
Sudhir0302 <141290194+Sudhir0302@users.noreply.github.com.>
2024-09-21 23:03:53 +0530

    created sidebar component

---
+++ src/Sidebar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className='flex justify-center items-center flex-col text-xl text-white mt-10'>
+        <div className='flex justify-normal mb-40'>
+          <h1>Search</h1>
+        </div>
+        <div>
+          <h1>users</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
